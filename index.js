cb73eed0c0647eab74ce2a3ecddf694c2aec7353
Rinor Rama <rinor.rama95@gmail.com>
2021-12-14 13:11:53 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,47 @@
+/* <header>
+	<div class="header-left">
+		<nav>
+			<ul class="header-left__list">
+				<li class="header-left__list-item">
+					<a class="header-left__link">
+						<h1 class="header-left__title">Hollixton</h1>
+					</a>
+				</li>
+				<li class="header-left__list-item">
+					<a class="header-left__link">Girls</a>
+				</li>
+				<li class="header-left__list-item">
+					<a class="header-left__link">Guys</a>
+				</li>
+				<li class="header-left__list-item">
+					<a class="header-left__link">Sale</a>
+				</li>
+			</ul>
+		</nav>
+	</div>
+	<div class="header-right">
+		<ul class="header-right__list">
+			<li class="header-right_list-item">
+				<button class="header-right__button">
+					<img src="search.svg" alt="search" />
+				</button>
+			</li>
+			<li class="header-right_list-item">
+				<button class="header-right__button">
+					<img src="profile.svg" alt="profile" />
+				</button>
+			</li>
+			<li class="header-right_list-item">
+				<button class="header-right__button">
+					<img src="bag.svg" alt="bag" />
+				</button>
+			</li>
+		</ul>
+	</div>
+</header>; */
+function renderHeader() {}
+
+function render() {
+	renderHeader();
+}
+render();
